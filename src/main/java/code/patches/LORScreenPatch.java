ddc805cfe4613abb192512ceca8c033508b682c0
Qingqi Wu <qw2341@tc.columbia.edu>
2024-02-25 17:50:39 +0800

    Added compatibility with LOR mod

---
+++ src/main/java/code/patches/LORScreenPatch.java
@@ -0,0 +1,19 @@
+package code.patches;
+
+import code.ProjectEMod;
+import code.ui.ExchangeScreen;
+import code.ui.TransmutationTable;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import lor.Utils;
+
+import java.util.ArrayList;
+
+@SpirePatch(clz = Utils.class, method = "openCardRewardsScreen", paramtypez = {ArrayList.class, boolean.class, int.class, String.class}, optional = true, requiredModId = "Library of Ruina")
+public class LORScreenPatch {
+    public static void Prefix(ArrayList<AbstractCard> cards, boolean allowSkip, int appearnum, String text) {
+        if(ExchangeScreen.show) TransmutationTable.exchangeScreen.close();
+        ProjectEMod.isScreenUp = true;
+    }
+
+}
