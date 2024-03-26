5d43e27857f565f1897b074fcb626fdb60583a9e
Qingqi Wu <qw2341@tc.columbia.edu>
2024-03-27 01:38:31 +0800

    Updated for LOR

--- src/main/java/code/patches/LORScreenPatch.java
+++ src/main/java/code/patches/LORScreenPatch.java
@@ -5,11 +5,11 @@ import code.ui.ExchangeScreen;
 import code.ui.TransmutationTable;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import lor.Utils;
+import lor.LORUtils;
 
 import java.util.ArrayList;
 
-@SpirePatch(clz = Utils.class, method = "openCardRewardsScreen", paramtypez = {ArrayList.class, boolean.class, int.class, String.class}, requiredModId = "Library of Ruina", optional = true)
+@SpirePatch(clz = LORUtils.class, method = "openCardRewardsScreen", paramtypez = {ArrayList.class, boolean.class, int.class, String.class}, requiredModId = "Library of Ruina", optional = true)
 public class LORScreenPatch {
     public static void Prefix(ArrayList<AbstractCard> cards, boolean allowSkip, int appearnum, String text) {
         if(ExchangeScreen.show) TransmutationTable.exchangeScreen.close();
